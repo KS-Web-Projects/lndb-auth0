ed1ecebddffff4f2c5a4f540fceb9b55cce8bdcf
Death-Knight-552 <discrilkx379@gmail.com>
2023-10-13 10:48:49 +0545

    added xata

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
-import Image from 'next/image'
+import { DownloadRecord, getXataClient } from '@/src/xata'
+import { RecordArray, SelectedPick } from '@xata.io/client';
 
-export default function Home() {
-  let data = [{
-    name: "The Apothecary Diaries",
-    link: "https://mega.nz/folder/P3IAGTLD#RFOo78yHfppMMqrPF2PfWQ"
-  }]
+// server side query
+
+const xata = getXataClient()
+
+export default async function Home()  {
+  
+  const { records: items, hasNextPage, nextPage} = await xata.db.download.getPaginated()
+  
   return (
     <div className='w-screen h-screen text-center'>
       <h1>Placeholder text</h1>
-      <form action=""><input type="text" name="search" id="search" className='w-10/12'/></form>
+      
       <div id="card-holder" className='mx-10 flex flex-col flex-wrap items-start'>
-        {data.map((item, id) => {
+        {items.length === 0 && <p>No Records Found</p>}
+        {items.map((item) => {
           return (
-            <div id='card' key={id}>
-              <a href={item.link} target='_blank'>
+            <div id='card' key={item.id}>
+              <a href={item.link || ""} target='_blank'>
 
               <h1>{item.name}</h1>
               </a>
@@ -21,6 +26,7 @@ export default function Home() {
           )
         })}
       </div>
+    
     </div>
   )
 }
